341b47a2355746abd3ff724d9e2c3f2f287a5204
Sergio Peña <sergiop79@gmail.com>
2014-12-09 11:10:15 +0100

    Initial commit

---
+++ Gulpfile.js
@@ -0,0 +1,67 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var minifyCSS = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var lr = require('tiny-lr');
+var livereload = require('gulp-livereload');
+var reloadServer = lr();
+
+
+// Styles
+gulp.task('styles', function () {
+  gulp.src('./scss/style.scss')
+    .pipe(sass({
+      errLogToConsole: true
+    }))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions']
+    }))
+    .pipe(minifyCSS())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./css'))
+    .pipe(livereload({
+      auto: false
+    }));
+});
+
+
+
+
+// index.html live reload
+gulp.task('html', function () {
+  gulp.src('./index.html')
+    .pipe(livereload({
+      auto: false
+    }));
+});
+
+
+// Server for development
+gulp.task('server', function () {
+  var http = require('http');
+  var st = require('node-static');
+  var opts = {
+    cache: false
+  };
+  var file = new st.Server('./', opts);
+  var port = process.env.PORT || 3000;
+
+  http.createServer(function (req, res) {
+    file.serve(req, res);
+  }).listen(port);
+
+  console.log('App running in http://localhost:%s', port);
+});
+
+
+// Watch files and run some tasks
+gulp.task('watch', function () {
+  livereload.listen();
+  gulp.watch(['./scss/**/*.scss'], ['styles']);
+  gulp.watch(['./index.html'], ['html']);
+});
+
+gulp.task('default', ['watch', 'styles', 'html', 'server']);
